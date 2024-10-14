ddd30a9c5130eac15b1bd776076080f9ad0f8e06
redarashid <redarashid824@gmail.com>
2024-10-14 22:18:47 +0300

    initial

--- src/components/residencies/Residencies.jsx
+++ src/components/residencies/Residencies.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
 import './Residencies.css'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css'
+import data from '../../utlis/slider.json'
 
 const Residencies = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='r-wraper'>
+        <div className='paddings innerWidth r-container'>
+            <div className='flexColStart r-head'>
+                <span className='orangeText'>Best Choices</span>
+                <span className='primaryText'>Popular Residencies</span>
+            </div>
+
+            <Swiper>
+                {
+                    data.map((card, i)=> (
+                        <SwiperSlide key={i}>
+                            <div className='r-card'>
+                                <img src={card.image} alt='home'/>
+
+                                <span className='secoundryText r-price'>
+                                    <span style={{ color: "orange" }}>$</span>
+                                    <span>{card.price}</span>
+
+                                    <span className='primaryText'>{card.name}</span>
+                                    <span className='secoundryText'>{card.detail}</span>
+                                </span>
+                            </div>
+                        </SwiperSlide>
+                    ))
+                }
+            </Swiper>
+        </div>
+    </section>
   )
 }
 
