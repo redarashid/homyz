3f6c77b3c690f89fa0cc231669b03a61960e8954
redarashid <redarashid824@gmail.com>
2024-10-15 15:21:32 +0300

    initial

--- src/components/residencies/Residencies.jsx
+++ src/components/residencies/Residencies.jsx
@@ -1,40 +1,94 @@
-import React from 'react'
-import './Residencies.css'
+// import React from 'react'
+// import './Residencies.css'
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import 'swiper/css'
+// import data from '../../utlis/slider.json'
+
+// const Residencies = () => {
+//   return (
+//     <section className='r-wraper'>
+//         <div className='paddings innerWidth r-container'>
+//             <div className='flexColStart r-head'>
+//                 <span className='orangeText'>Best Choices</span>
+//                 <span className='primaryText'>Popular Residencies</span>
+//             </div>
+
+//             <Swiper>
+//                 {
+//                     data.map((card, i)=> (
+//                         <SwiperSlide key={i}>
+//                             <div className='r-card'>
+//                                 <img src={card.image} alt='home'/>
+
+//                                 <span className='secoundryText r-price'>
+//                                     <span style={{ color: "orange" }}>$</span>
+//                                     <span>{card.price}</span>
+
+//                                     <span className='primaryText'>{card.name}</span>
+//                                     <span className='secoundryText'>{card.detail}</span>
+//                                 </span>
+//                             </div>
+//                         </SwiperSlide>
+//                     ))
+//                 }
+//             </Swiper>
+//         </div>
+//     </section>
+//   )
+// }
+
+// export default Residencies
+
+
+
+
+
+
+
+
+
+
+
+import React from 'react';
+import './Residencies.css';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css'
-import data from '../../utlis/slider.json'
+import 'swiper/css';
+import data from '../../utlis/slider.json';
+import {sliderSettings} from '../../utlis/common'
 
 const Residencies = () => {
   return (
-    <section className='r-wraper'>
-        <div className='paddings innerWidth r-container'>
-            <div className='flexColStart r-head'>
-                <span className='orangeText'>Best Choices</span>
-                <span className='primaryText'>Popular Residencies</span>
-            </div>
-
-            <Swiper>
-                {
-                    data.map((card, i)=> (
-                        <SwiperSlide key={i}>
-                            <div className='r-card'>
-                                <img src={card.image} alt='home'/>
-
-                                <span className='secoundryText r-price'>
-                                    <span style={{ color: "orange" }}>$</span>
-                                    <span>{card.price}</span>
-
-                                    <span className='primaryText'>{card.name}</span>
-                                    <span className='secoundryText'>{card.detail}</span>
-                                </span>
-                            </div>
-                        </SwiperSlide>
-                    ))
-                }
-            </Swiper>
+    <section className='r-wrapper'>
+      <div className='paddings innerWidth r-container'>
+        <div className='flexColStart r-head'>
+          <span className='orangeText'>Best Choices</span>
+          <span className='primaryText'>Popular Residencies</span>
         </div>
-    </section>
-  )
-}
 
-export default Residencies
+        <Swiper {...sliderSettings}>
+            <SliderButtons/>
+          {data.map((card, i) => (
+            <SwiperSlide key={i}>
+              <div className='r-card'>
+                <img src={card.image} alt='home' className='r-card-image' />
+                <div className='r-card-content'>
+                  <span className='secondaryText r-price'>
+                    <span style={{ color: "orange" }}>$</span>
+                    <span>{card.price}</span>
+                  </span>
+                  <span className='primaryText'>{card.name}</span>
+                  <span className='secondaryText'>{card.detail}</span>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </section>
+  );
+};
+
+export default Residencies;
+
+
+const SliderButtons = () 
