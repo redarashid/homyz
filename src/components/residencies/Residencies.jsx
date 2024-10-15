6d1a497f6aba515e5b9cb156f75edc87565c6f26
redarashid <redarashid824@gmail.com>
2024-10-15 15:47:50 +0300

    initial

--- src/components/residencies/Residencies.jsx
+++ src/components/residencies/Residencies.jsx
@@ -1,60 +1,73 @@
-// import React from 'react'
-// import './Residencies.css'
+
+// import React from 'react';
+// import './Residencies.css';
 // import { Swiper, SwiperSlide } from 'swiper/react';
-// import 'swiper/css'
-// import data from '../../utlis/slider.json'
+// import 'swiper/css';
+// import data from '../../utlis/slider.json';
+// import {sliderSettings} from '../../utlis/common'
+// import { Navigation } from 'swiper';
+// import 'swiper/swiper-bundle.css';
+
 
 // const Residencies = () => {
 //   return (
-//     <section className='r-wraper'>
-//         <div className='paddings innerWidth r-container'>
-//             <div className='flexColStart r-head'>
-//                 <span className='orangeText'>Best Choices</span>
-//                 <span className='primaryText'>Popular Residencies</span>
-//             </div>
-
-//             <Swiper>
-//                 {
-//                     data.map((card, i)=> (
-//                         <SwiperSlide key={i}>
-//                             <div className='r-card'>
-//                                 <img src={card.image} alt='home'/>
-
-//                                 <span className='secoundryText r-price'>
-//                                     <span style={{ color: "orange" }}>$</span>
-//                                     <span>{card.price}</span>
-
-//                                     <span className='primaryText'>{card.name}</span>
-//                                     <span className='secoundryText'>{card.detail}</span>
-//                                 </span>
-//                             </div>
-//                         </SwiperSlide>
-//                     ))
-//                 }
-//             </Swiper>
+//     <section className='r-wrapper'>
+//       <div className='paddings innerWidth r-container'>
+//         <div className='flexColStart r-head'>
+//           <span className='orangeText'>Best Choices</span>
+//           <span className='primaryText'>Popular Residencies</span>
 //         </div>
+
+//         <Swiper {...sliderSettings}>
+//             <SliderWithButtons/>
+//           {data.map((card, i) => (
+//             <SwiperSlide key={i}>
+//               <div className='r-card'>
+//                 <img src={card.image} alt='home' className='r-card-image' />
+//                 <div className='r-card-content'>
+//                   <span className='secondaryText r-price'>
+//                     <span style={{ color: "orange" }}>$</span>
+//                     <span>{card.price}</span>
+//                   </span>
+//                   <span className='primaryText'>{card.name}</span>
+//                   <span className='secondaryText'>{card.detail}</span>
+//                 </div>
+//               </div>
+//             </SwiperSlide>
+//           ))}
+//         </Swiper>
+//       </div>
 //     </section>
-//   )
+//   );
+// };
+
+// export default Residencies;
+
+
+// const SliderWithButtons = () => {
+//   return (
+//     <Swiper
+//       modules={[Navigation]} 
+//       navigation                   
+//       spaceBetween={50}
+//       slidesPerView={1}
+//     >
+//       <SwiperSlide><div>Slide 1</div></SwiperSlide>
+//       <SwiperSlide><div>Slide 2</div></SwiperSlide>
+//       <SwiperSlide><div>Slide 3</div></SwiperSlide>
+//     </Swiper>
+//   );
 // }
-
-// export default Residencies
-
-
-
-
-
-
-
-
-
 
 
 import React from 'react';
 import './Residencies.css';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
+import 'swiper/css/navigation';    
 import data from '../../utlis/slider.json';
-import {sliderSettings} from '../../utlis/common'
+import { sliderSettings } from '../../utlis/common';
+import { Navigation } from 'swiper/modules';     
 
 const Residencies = () => {
   return (
@@ -65,8 +78,11 @@ const Residencies = () => {
           <span className='primaryText'>Popular Residencies</span>
         </div>
 
-        <Swiper {...sliderSettings}>
-            <SliderButtons/>
+        <Swiper
+          {...sliderSettings}
+          modules={[Navigation]}     
+          navigation             
+        >
           {data.map((card, i) => (
             <SwiperSlide key={i}>
               <div className='r-card'>
@@ -89,6 +105,3 @@ const Residencies = () => {
 };
 
 export default Residencies;
-
-
-const SliderButtons = () 
